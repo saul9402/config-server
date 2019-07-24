dd07c34497ae9bbb2ba33f1dc06a450a8120b401
Saul Avila <saul_harry_@hotmail.com>
2019-07-24 11:29:18 -0500

    commit inicial

---
+++ src/main/java/com/formacionbdi/springboot/app/config/SpringbootServicioConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.formacionbdi.springboot.app.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/*
+ * Con esta anotación habilitas el proyecto como servidor de configuración
+ *
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class SpringbootServicioConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioConfigServerApplication.class, args);
+	}
+
+}
